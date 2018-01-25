f30ae45219cce05513e34f1a6bf22c507683b445
Yanyu Liang <yanyul@uchicago.edu>
2018-01-25 17:13:03 -0600

    coding

--- data/genFakeData.R
+++ data/genFakeData.R
@@ -1,11 +1,13 @@
 library(optparse)
 
-help_text = 'This script generate two files: 1. BIMBAM mean genotype file, 2. SNP information file. The SNP info file takes the followling format:
+help_text = 'This script generate a collection ([nchr] pairs) of paired two files: 1. BIMBAM mean genotype file, 2. SNP information file. The SNP info file takes the followling format:
   [snp-id]\t[position]\t[chr-num]
 Note that 10% of mean genotype will be non-integer'
 
 option_list = list(
-  make_option(c("-s", "--nsnps"), type="numeric", default=20,
+  make_option(c('-c', '--nchr'), type="numeric", default=22,
+              help="the number of chromosomes [default = %default]", metavar="character"),
+  make_option(c("-s", "--nsnp"), type="numeric", default=20,
               help="the number of snps [default = %default]", metavar="character"),
   make_option(c("-i", "--nindividuals"), type="numeric", default=200,
               help="the number of individuals [default = %default]", metavar="character"),
@@ -15,22 +17,6 @@ option_list = list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
-geno.mean.out <- paste0(opt$out, '.genotype-mean.txt.gz')
-snp.info.out <- paste0(opt$out, '.snp.pos.gz')
-snp.names <- paste0('snp', 1 : opt$nsnps)
-snp.chr <- sample(22, opt$nsnps, replace = T)
-snp.pos <- round(runif(opt$nsnps) * 1e6)
-ordered.snp <- data.frame(x1 = snp.chr, x2 = snp.pos)
-ordered.snp <- ordered.snp[order(ordered.snp[, 1], ordered.snp[, 2]), ]
-snp <- data.frame(snp.names, ordered.snp$x1, ordered.snp$x2)
-bases <- c('A', 'T', 'C', 'G')
-allele.set <- c()
-for(i in bases) {
-  for(j in bases) {
-    if(i == j) next
-    allele.set <- c(allele.set, paste(i, j))
-  }
-}
 genGeno <- function(ni, snp.name, allele.set) {
   allele <- strsplit(allele.set[sample(length(allele.set), 1)], ' ')[[1]]
   out <- sample(3, ni, replace = T) - 1
@@ -41,10 +27,29 @@ genGeno <- function(ni, snp.name, allele.set) {
   snp.name <- as.character(snp.name)
   return(c(snp.name, allele, format(round(out, digits = 3), nsmall = 3)))
 }
-geno <- t(sapply(snp[,1], genGeno, ni=opt$nindividuals, allele.set=allele.set))
-gz1 <- gzfile(geno.mean.out, 'w')
-write.table(geno, gz1, quote = F, col.names = F, row.names = F)
-close(gz1)
-gz2 <- gzfile(snp.info.out, 'w')
-write.table(snp, gz2, sep = '\t', quote = F, col.names = F, row.names = F)
-close(gz2)
+
+for(c in 1 : opt$nchr) {
+  geno.mean.out <- paste0(opt$out, '.genotype-mean', 'chr', c, '.txt.gz')
+  snp.info.out <- paste0(opt$out, '.snp-list', 'chr', c, '.gz')
+  snp.names <- paste0('snp', 1 : opt$nsnp)
+  snp.chr <- rep(c, opt$nsnp) # sample(22, opt$nsnp, replace = T)
+  snp.pos <- round(runif(opt$nsnp) * 1e6)
+  ordered.snp <- data.frame(x1 = snp.chr, x2 = snp.pos)
+  ordered.snp <- ordered.snp[order(ordered.snp[, 1], ordered.snp[, 2]), ]
+  snp <- data.frame(snp.names, ordered.snp$x1, ordered.snp$x2)
+  bases <- c('A', 'T', 'C', 'G')
+  allele.set <- c()
+  for(i in bases) {
+    for(j in bases) {
+      if(i == j) next
+      allele.set <- c(allele.set, paste(i, j))
+    }
+  }
+  geno <- t(sapply(snp[,1], genGeno, ni=opt$nindividuals, allele.set=allele.set))
+  gz1 <- gzfile(geno.mean.out, 'w')
+  write.table(geno, gz1, quote = F, col.names = F, row.names = F)
+  close(gz1)
+  gz2 <- gzfile(snp.info.out, 'w')
+  write.table(snp, gz2, sep = '\t', quote = F, col.names = F, row.names = F)
+  close(gz2)
+}
--- data/gen_fake_data.sh
+++ data/gen_fake_data.sh
@@ -1,3 +1,3 @@
 # generate fake data for testing
 # 01/24/18
-Rscript genFakeData.R -s 50 -i 100 -o test1
+Rscript genFakeData.R -c 5 -s 50 -i 200 -o test1
