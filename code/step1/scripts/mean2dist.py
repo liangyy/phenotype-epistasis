b0c21b6e41488449cd06dbc6f638dcd267f16633
Yanyu Liang <yanyul@midway2-login2.rcc.local>
2018-02-09 19:00:13 -0600

    made mean2dist.py faster

--- code/step1/scripts/mean2dist.py
+++ code/step1/scripts/mean2dist.py
@@ -9,6 +9,7 @@ parser.add_argument('--input', help = 'input genotype mean file in GZ format')
 parser.add_argument('--output', help = 'file name of output genotype distribution file in GZ format')
 args = parser.parse_args()
 
+import sys
 import pandas as pd
 import numpy as np
 
@@ -16,15 +17,28 @@ geno = pd.read_table(args.input, sep = ' ', header = None, compression = 'gzip')
 geno_info = geno.iloc[:, [0, 2, 1]]
 geno_mean = geno.iloc[:, 3:]
 (ns, ni) = geno_mean.shape
-geno_dist = pd.DataFrame()
+geno_dist = np.empty([ns, ni * 2])  # pd.DataFrame()
 distance = 0
 for i in range(ni): # iter over every individual
+    # print(str(i) + '--a')
     geno_mean_i = round(geno_mean.iloc[:, i])
+    # print(str(i) + '-a')
     distance += sum(abs(geno_mean_i - geno_mean.iloc[:, i]))
+    # print(str(i) + 'a')
     geno_dist_i = pd.DataFrame(np.zeros((ns, 2)))
+    # print(str(i) + 'b')
     geno_dist_i.ix[geno_mean_i == 0, 0] = 1
+    # print(str(i) + 'c')
     geno_dist_i.ix[geno_mean_i == 1, 1] = 1
-    geno_dist = pd.concat([geno_dist, geno_dist_i], axis = 1)
-geno_out = pd.concat([geno_info, geno_dist], axis = 1)
-print('average distance between posterior mean genotype and integerized genotype is {avg_distance}'.format(avg_distance = distance / ns / ni))
-geno_out.to_csv(args.output, sep = ' ', header = False, index = False, compression = 'gzip')
+    # print(str(i) + 'd')
+    geno_dist[:, i * 2 : i * 2 + 2] = geno_dist_i  # np.concatenate([geno_dist, geno_dist_i], axis = 1)
+    # print(i)
+# for i in range(ns):
+    
+geno_out = pd.concat([geno_info, pd.DataFrame(geno_dist)], axis = 1)
+print('average distance between posterior mean genotype and integerized genotype is {avg_distance}'.format(avg_distance = distance / ns / ni), file=sys.stderr)
+
+def print_row(row):
+    print(' '.join([ str(i) for i in row ]))
+geno_out.apply(print_row, axis=1)
+# geno_out.to_csv(args.output, sep = ' ', header = False, index = False, compression = 'gzip', chunksize=100000)
