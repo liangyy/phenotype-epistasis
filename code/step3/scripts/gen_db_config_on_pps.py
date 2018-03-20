d9f874120e97d47f676c9b1099f76579a9e6edaa
Yanyu Liang <yanyul@uchicago.edu>
2018-03-20 11:56:45 -0500

    added a script to help prepare config for gene-based analysis

---
+++ code/step3/scripts/gen_db_config_on_pps.py
@@ -0,0 +1,21 @@
+import argparse
+parser = argparse.ArgumentParser(prog='gen_db_config_on_pps.py', description='''
+    This script in written to generate proper format for PrediXcan databases on pps cluster
+    The working dir should contain folders of PrediXcan databases in each sub dir
+''')
+parser.add_argument('--work_dir', help = 'the dir of database sib dirs. DO NOT use relative path!')
+args = parser.parse_args()
+
+import os, glob
+from os.path import expanduser
+
+wd = expanduser(args.work_dir)
+
+e = glob.glob(wd +  '/*')
+for i in e:
+    if os.path.isdir(i):
+        f = glob.glob(i + '/*')
+        for d in f:
+            if d.endswith('.db'):
+                string = '{name}:\n  db: {path}'.format(name = os.path.basename(i), path = d)
+                print(string)
